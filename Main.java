34a5d6a16ddac9c7657c7acc439fbabc59d7f1bb
olegstr1 <116093656+olegstr1@users.noreply.github.com>
2022-10-18 17:43:54 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введи своє (або не своє) ім'я!\n"+
+                "Можеш ввести ім'я Оксани...\n"+
+                "Можеш ввести його ангійською....\n");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(Spain(name));
+        System.out.println(Germany(name));
+        System.out.println(IsOksana(name));
+    }
+
+    public static String Spain (String a) {
+        String s = "Hola," + a;
+        return s;
+    }
+
+    public static String Germany (String a) {
+        String s = "Halo," + a;
+        return s;
+    }
+
+    public static String IsOksana (String a) {
+        String s = new String();
+        if(a.equals("Oksana")){
+            s = "Wow! You are Oksana!";
+        }
+        else{
+            s = "Meh... You are not Oksana...";
+        }
+        return s;
+    }
+
+}
